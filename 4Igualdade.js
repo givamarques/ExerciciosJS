6e777bc39edcb164a9995efcdc11874c23f1e1ac
givamarques <49195218+givamarques@users.noreply.github.com>
2022-01-18 15:10:09 -0300

    Create 4Igualdade.js

---
+++ 4Igualdade.js
@@ -0,0 +1,8 @@
+//Operadores de igualdade
+//Igualdade Estrita
+console.log( 1 === 1); //true pois o valor e tipo sao iguais
+console.log ( '1' === 1 ); //false pois tipos sao diferentes
+
+//Igualdade Solta
+console.log( 1 == 1); //true pois o valor e tipo sao iguais
+console.log ( '1' == 1 ); //true pois o valor é igual apesar dos tipos diferentes
