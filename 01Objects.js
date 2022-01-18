ec46cda2e36d130a05a2645d0c1faa11c239ef23
givamarques <49195218+givamarques@users.noreply.github.com>
2022-01-18 14:12:42 -0300

    Create 01Objects.js

---
+++ 01Objects.js
@@ -0,0 +1,14 @@
+let nome = 'Rafael';
+let idade = 25;
+let estaAprovado = true;
+let sobrenome = undefined;
+
+//Objeto Pessoa
+let pessoa = {
+  nome: 'Rafael',
+  idade: 25,
+  estaAprovado: true,
+  sobrenome: 'de Souza',
+};
+
+console.log(pessoa);
